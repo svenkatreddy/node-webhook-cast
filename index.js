f944f475141e19ebaa7ce2150d1a35df54980267
svenkatreddy <svenkatreddy@users.noreply.github.com>
2020-01-02 17:00:02 -0800

    Add mechanism to get music an cast to device chosen

--- index.js
+++ index.js
@@ -3,27 +3,53 @@ const dependenyInstall = require('./lib/dependency-install');
 const listDevices = require('./lib/list-devices');
 const server = require('./lib/server');
 const tunnel = require('./lib/tunnel');
+const downloadTopPop = require('./lib/download-top-pop-songs');  
 const castToDevice = require('./lib/cast-to-device');  
+const getFiles =  require('./lib/get-cast-files');
 
 const port = process.env.PORT || 4545;
+const castDevice = process.env.CASTDEVICE || 'Living Room TV';
 
-function handleWebHook(req, res, next) {
+const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
+}
+
+async function handleWebHook(req, res, next) {
   const { url } = req;
+  console.log(`someone requested ${url}`);
   switch (url) {
-    case '/' : {
-      castToDevice('Living room speaker', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    case '/webhook-on-closing-door' : {
+      res.status(200).send('ok');
+      const files = getFiles();
+      await asyncForEach(files, async (file) => {
+        await castToDevice(castDevice, file, 'local');
+      })
     }
   }
   return next();
 };
 
 async function start () {
+  // install required dependencies of pip3, python, youtube-dl
   dependenyInstall();
+  
+  // get list of devices available
   const devices = listDevices();
   console.log(devices);
+
+  // start webserver to receive webhook connection.
   server(port, handleWebHook);
-  const url = await tunnel(port);
-  console.log(url);
+
+  // run once every week
+  setInterval(async function () {
+    await downloadTopPop();
+  }, 604800);
+
+  // Either portforward it or use tunnel from localtunnel.
+  // const url = await tunnel(port);
+  // console.log(url);
 }
 
 return start();
